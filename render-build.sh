3c825fa737e24d8521577df52c62f59c01c43449
Rafael Acorinti <rafaelacorinti@gmail.com>
2026-06-18 10:47:06 -0300

    fix: disable eslint/typescript strict build, fix render-build error detection, fix dependencies

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 npm install
 npx next build
+ls -la .next/standalone/server.js || (echo "Build failed - standalone not found" && exit 1)
 mkdir -p .next/standalone/.next
 cp -r .next/static .next/standalone/.next/static
 mkdir -p .next/standalone/public
